c05faba4b3462dece17ce6958272e0097f480405
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-06-30 13:36:26 +0300

    Add files via upload
    Added ability to display default dictionary components (places), may now remove selected and add new places

--- CSharp-List/CSharp-List/Form1.Designer.cs
+++ CSharp-List/CSharp-List/Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.NameBox = new System.Windows.Forms.TextBox();
             this.CrtListBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.CrtDictBtn = new System.Windows.Forms.Button();
+            this.ClrTxtBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -42,14 +44,14 @@
             this.textBox1.Location = new System.Drawing.Point(40, 33);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(395, 284);
+            this.textBox1.Size = new System.Drawing.Size(406, 348);
             this.textBox1.TabIndex = 0;
             // 
             // AddBtn
             // 
-            this.AddBtn.Location = new System.Drawing.Point(503, 92);
+            this.AddBtn.Location = new System.Drawing.Point(494, 141);
             this.AddBtn.Name = "AddBtn";
-            this.AddBtn.Size = new System.Drawing.Size(88, 31);
+            this.AddBtn.Size = new System.Drawing.Size(97, 35);
             this.AddBtn.TabIndex = 2;
             this.AddBtn.Text = "Add";
             this.AddBtn.UseVisualStyleBackColor = true;
@@ -57,9 +59,9 @@
             // 
             // RemBtn
             // 
-            this.RemBtn.Location = new System.Drawing.Point(503, 160);
+            this.RemBtn.Location = new System.Drawing.Point(494, 194);
             this.RemBtn.Name = "RemBtn";
-            this.RemBtn.Size = new System.Drawing.Size(88, 31);
+            this.RemBtn.Size = new System.Drawing.Size(97, 35);
             this.RemBtn.TabIndex = 3;
             this.RemBtn.Text = "Remove";
             this.RemBtn.UseVisualStyleBackColor = true;
@@ -67,9 +69,9 @@
             // 
             // ShowBtn
             // 
-            this.ShowBtn.Location = new System.Drawing.Point(503, 229);
+            this.ShowBtn.Location = new System.Drawing.Point(494, 245);
             this.ShowBtn.Name = "ShowBtn";
-            this.ShowBtn.Size = new System.Drawing.Size(88, 31);
+            this.ShowBtn.Size = new System.Drawing.Size(97, 35);
             this.ShowBtn.TabIndex = 4;
             this.ShowBtn.Text = "Show";
             this.ShowBtn.UseVisualStyleBackColor = true;
@@ -77,35 +79,57 @@
             // 
             // NameBox
             // 
-            this.NameBox.Location = new System.Drawing.Point(503, 297);
+            this.NameBox.Location = new System.Drawing.Point(494, 361);
             this.NameBox.Name = "NameBox";
-            this.NameBox.Size = new System.Drawing.Size(88, 20);
+            this.NameBox.Size = new System.Drawing.Size(97, 20);
             this.NameBox.TabIndex = 5;
             // 
             // CrtListBtn
             // 
-            this.CrtListBtn.Location = new System.Drawing.Point(503, 33);
+            this.CrtListBtn.Location = new System.Drawing.Point(494, 33);
             this.CrtListBtn.Name = "CrtListBtn";
-            this.CrtListBtn.Size = new System.Drawing.Size(88, 31);
+            this.CrtListBtn.Size = new System.Drawing.Size(97, 35);
             this.CrtListBtn.TabIndex = 6;
-            this.CrtListBtn.Text = "Create List";
+            this.CrtListBtn.Text = "Create List (Item)";
             this.CrtListBtn.UseVisualStyleBackColor = true;
             this.CrtListBtn.Click += new System.EventHandler(this.CrtListBtn_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(139, 9);
+            this.label1.Location = new System.Drawing.Point(99, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 13);
+            this.label1.Size = new System.Drawing.Size(284, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "oldschool runescape item list generator";
+            this.label1.Text = "oldschool runescape  list (item) and dictionary (place) editor";
+            // 
+            // CrtDictBtn
+            // 
+            this.CrtDictBtn.Location = new System.Drawing.Point(494, 88);
+            this.CrtDictBtn.Name = "CrtDictBtn";
+            this.CrtDictBtn.Size = new System.Drawing.Size(97, 35);
+            this.CrtDictBtn.TabIndex = 8;
+            this.CrtDictBtn.Text = "Create Dictionary (place)";
+            this.CrtDictBtn.UseVisualStyleBackColor = true;
+            this.CrtDictBtn.Click += new System.EventHandler(this.CrtDictBtn_Click);
+            // 
+            // ClrTxtBtn
+            // 
+            this.ClrTxtBtn.Location = new System.Drawing.Point(494, 304);
+            this.ClrTxtBtn.Name = "ClrTxtBtn";
+            this.ClrTxtBtn.Size = new System.Drawing.Size(97, 35);
+            this.ClrTxtBtn.TabIndex = 9;
+            this.ClrTxtBtn.Text = "Clear Text";
+            this.ClrTxtBtn.UseVisualStyleBackColor = true;
+            this.ClrTxtBtn.Click += new System.EventHandler(this.ClrTxtBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(637, 365);
+            this.ClientSize = new System.Drawing.Size(655, 417);
+            this.Controls.Add(this.ClrTxtBtn);
+            this.Controls.Add(this.CrtDictBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.CrtListBtn);
             this.Controls.Add(this.NameBox);
@@ -129,6 +153,8 @@
         private System.Windows.Forms.TextBox NameBox;
         private System.Windows.Forms.Button CrtListBtn;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CrtDictBtn;
+        private System.Windows.Forms.Button ClrTxtBtn;
     }
 }
 
--- CSharp-List/CSharp-List/Form1.cs
+++ CSharp-List/CSharp-List/Form1.cs
@@ -16,12 +16,36 @@ namespace CSharp_List
         {
             InitializeComponent();
         }
+        //created objects
         public List<Item> itemList = new List<Item>();
+        public Dictionary<string, Place> placeDictionary = new Dictionary<string, Place>();
+
+        public class Item
+        {
+            public string name;
+            public string description;
+
+            public Item(string aName, string aDescription)
+            {
+                name = aName;
+                description = aDescription;
+            }
+        }
+
+        public class Place
+        {
+            public string description;
+
+            public Place(string aDescription)
+            {
+                description = aDescription;
+            }
+        }
 
         private void CrtListBtn_Click(object sender, EventArgs e)
         {
-            if(itemList.Count == 0)
-            {
+            if (itemList.Count == 0) {
+                //default items
                 itemList.Add(new Item("Dragonfire shield", "A heavy shield with a snarling, draconic visage"));
                 itemList.Add(new Item("Zulrah's scales", "Flakes of toxic snakeskin."));
                 itemList.Add(new Item("Abyssal whip", "A weapon from the abyss."));
@@ -31,49 +55,71 @@ namespace CSharp_List
                 itemList.Add(new Item("Ring of the gods", "An ancient ring said to bring you closer to the Gods."));
                 itemList.Add(new Item("Dragon chainbody", "	A series of connected metal rings."));
             }
-            else
-            {
+            else {
                 //escape r with single \'s caused Unrecognized escape sequence error
                 textBox1.AppendText("\\r\\itemList is empty");
             }
         }
 
+        private void CrtDictBtn_Click(object sender, EventArgs e)
+        {
+            if (placeDictionary.Count == 0) {
+                //default places
+                placeDictionary.Add("King Black Dragon Lair", new Place("A lair guarded by the three-headed King Black Dragon"));
+                placeDictionary.Add("Grand Exchange", new Place("The biggest player market place in Oldschool Runescape"));
+                placeDictionary.Add("Jaleustrophos Pyramid", new Place("An Agility training area located in the Kharidian Desert"));
+                placeDictionary.Add("Draynor Village", new Place("Draynor Village is a small village located between Falador and Lumbridge"));
+                placeDictionary.Add("Zulrah's Shrine", new Place("Zulrah's monument to the east of Zul-Andra"));
+                placeDictionary.Add("Kraken Cove", new Place("Dungeon located south-west of the Piscatoris Fishing Colony"));
+                placeDictionary.Add("Canifis", new Place("Canifis is a small town in Morytania"));
+                placeDictionary.Add("Edgeville Monastery", new Place("A two-story building north-east of Falador"));
+            }
+            else {
+                //escape r with single \'s caused Unrecognized escape sequence error
+                textBox1.AppendText("\\r\\placeDictionary is empty");
+            }
+        }
         private void AddBtn_Click(object sender, EventArgs e)
         {
             itemList.Add(new Item("New Item", "A new thing"));
+            placeDictionary.Add("New Place", new Place("A nice new place"));
         }
 
         private void RemBtn_Click(object sender, EventArgs e)
         {
             string searchname = NameBox.Text;
-            for(int i = 0; i < itemList.Count; i++)
-            {
-                itemList.RemoveAt(i);
-                textBox1.AppendText(searchname + " removed from itemList \r\n");
-                break;
+            if (placeDictionary.ContainsKey(searchname)) {
+                placeDictionary.Remove(searchname);
+                textBox1.AppendText(searchname + " removed from placeDictionary.\r\n");
+            }
+            else {
+                textBox1.AppendText(searchname + " not found in placeDictionary.\r\n");
+            }
+            for (int i = 0; i < itemList.Count; i++) {
+                if(itemList[i].name == searchname) {
+                    itemList.RemoveAt(i);
+                    textBox1.AppendText(searchname + " removed from itemList \r\n");
+                    break;
+                }
             }
         }
 
         private void ShowBtn_Click(object sender, EventArgs e)
         {
-            string s = "~~~~~~~~~~~~~~~~~~~~~~~~~~Items~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n";
-            foreach (Item th in itemList)
-            {
+            string s = "~~~~~~~~~~~~~~~~~~~~~~~~~~~Items~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n";
+            foreach (Item th in itemList) {
                 s += String.Format("{0}: {1}\r\n", th.name, th.description);
+            }
+            s += "~~~~~~~~~~~~~~~~~~~~~~~~~~Places~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n";
+            foreach(KeyValuePair<string,Place> keyvaluepair in placeDictionary) {
+                s += String.Format("{0}: {1}\r\n", keyvaluepair.Key, keyvaluepair.Value.description);
             }
             textBox1.Text = s;
         }
-    }
 
-    public class Item
-    {
-        public string name;
-        public string description;
-
-        public Item(string aName, string aDescription)
+        private void ClrTxtBtn_Click(object sender, EventArgs e)
         {
-            name = aName;
-            description = aDescription;
+            textBox1.Text = "";
         }
     }
 }
