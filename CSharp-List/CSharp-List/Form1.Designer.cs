5bd7d718a54a3bad6000fe01e5f80fd63c8d26f4
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-06-30 12:29:31 +0300

    Add files via upload
    Can view default oldschool runescape items, remove and add new items (soon)

---
+++ CSharp-List/CSharp-List/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CSharp_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.RemBtn = new System.Windows.Forms.Button();
+            this.ShowBtn = new System.Windows.Forms.Button();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.CrtListBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(395, 284);
+            this.textBox1.TabIndex = 0;
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(503, 92);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(88, 31);
+            this.AddBtn.TabIndex = 2;
+            this.AddBtn.Text = "Add";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // RemBtn
+            // 
+            this.RemBtn.Location = new System.Drawing.Point(503, 160);
+            this.RemBtn.Name = "RemBtn";
+            this.RemBtn.Size = new System.Drawing.Size(88, 31);
+            this.RemBtn.TabIndex = 3;
+            this.RemBtn.Text = "Remove";
+            this.RemBtn.UseVisualStyleBackColor = true;
+            this.RemBtn.Click += new System.EventHandler(this.RemBtn_Click);
+            // 
+            // ShowBtn
+            // 
+            this.ShowBtn.Location = new System.Drawing.Point(503, 229);
+            this.ShowBtn.Name = "ShowBtn";
+            this.ShowBtn.Size = new System.Drawing.Size(88, 31);
+            this.ShowBtn.TabIndex = 4;
+            this.ShowBtn.Text = "Show";
+            this.ShowBtn.UseVisualStyleBackColor = true;
+            this.ShowBtn.Click += new System.EventHandler(this.ShowBtn_Click);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(503, 297);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(88, 20);
+            this.NameBox.TabIndex = 5;
+            // 
+            // CrtListBtn
+            // 
+            this.CrtListBtn.Location = new System.Drawing.Point(503, 33);
+            this.CrtListBtn.Name = "CrtListBtn";
+            this.CrtListBtn.Size = new System.Drawing.Size(88, 31);
+            this.CrtListBtn.TabIndex = 6;
+            this.CrtListBtn.Text = "Create List";
+            this.CrtListBtn.UseVisualStyleBackColor = true;
+            this.CrtListBtn.Click += new System.EventHandler(this.CrtListBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "oldschool runescape item list generator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 365);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CrtListBtn);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.ShowBtn);
+            this.Controls.Add(this.RemBtn);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button RemBtn;
+        private System.Windows.Forms.Button ShowBtn;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button CrtListBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CSharp-List/CSharp-List/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<Item> itemList = new List<Item>();
+
+        private void CrtListBtn_Click(object sender, EventArgs e)
+        {
+            if(itemList.Count == 0)
+            {
+                itemList.Add(new Item("Dragonfire shield", "A heavy shield with a snarling, draconic visage"));
+                itemList.Add(new Item("Zulrah's scales", "Flakes of toxic snakeskin."));
+                itemList.Add(new Item("Abyssal whip", "A weapon from the abyss."));
+                itemList.Add(new Item("Fire cape", "A cape of fire."));
+                itemList.Add(new Item("Robin hood hat", "Endorsed by Robin Hood."));
+                itemList.Add(new Item("Amulet of fury", "A very powerful onyx amulet."));
+                itemList.Add(new Item("Ring of the gods", "An ancient ring said to bring you closer to the Gods."));
+                itemList.Add(new Item("Dragon chainbody", "	A series of connected metal rings."));
+            }
+            else
+            {
+                //escape r with single \'s caused Unrecognized escape sequence error
+                textBox1.AppendText("\\r\\itemList is empty");
+            }
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            itemList.Add(new Item("New Item", "A new thing"));
+        }
+
+        private void RemBtn_Click(object sender, EventArgs e)
+        {
+            string searchname = NameBox.Text;
+            for(int i = 0; i < itemList.Count; i++)
+            {
+                itemList.RemoveAt(i);
+                textBox1.AppendText(searchname + " removed from itemList \r\n");
+                break;
+            }
+        }
+
+        private void ShowBtn_Click(object sender, EventArgs e)
+        {
+            string s = "~~~~~~~~~~~~~~~~~~~~~~~~~~Items~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n";
+            foreach (Item th in itemList)
+            {
+                s += String.Format("{0}: {1}\r\n", th.name, th.description);
+            }
+            textBox1.Text = s;
+        }
+    }
+
+    public class Item
+    {
+        public string name;
+        public string description;
+
+        public Item(string aName, string aDescription)
+        {
+            name = aName;
+            description = aDescription;
+        }
+    }
+}
